c4dafcd0fbb5aeeb9aae2f1af3208787ac8254ce
baysicinglish <dbeazley.400@gmail.com>
2018-12-07 14:29:29 +0000

    Added test scripts

---
+++ script_test.py
@@ -0,0 +1,9 @@
+from script import Greeting
+import pytest
+
+def test_returns_input():
+    assert Greeting.greet('Dan') == 'Hello, Dan'
+
+def test_raises_exception_on_non_string_arguments():
+    with pytest.raises(TypeError):
+        Greeting.greet(9)
