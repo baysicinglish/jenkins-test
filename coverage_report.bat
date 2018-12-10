338d7f7028992b12fe75d2b867712cbaff4f3b73
baysicinglish <dbeazley.400@gmail.com>
2018-12-10 15:44:51 +0000

    added html functionality

--- coverage_report.bat
+++ coverage_report.bat
@@ -3,5 +3,6 @@ pip install pytest
 pip install coverage
 coverage run script_test.py
 coverage report -m
-coverage xml -o coverage_report
+coverage xml -o coverage_report.xml
+coverage html -o coverage_report.html
 rmvirtualenv jenkins_test
