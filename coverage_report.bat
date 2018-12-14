dcb361142cde49d4491b994670d7fc79b1349b53
baysicinglish <dbeazley.400@gmail.com>
2018-12-14 15:39:48 +0000

    removed previous venv code

--- coverage_report.bat
+++ coverage_report.bat
@@ -1,8 +1,6 @@
-call mkvirtualenv jenkins_test
 pip install pytest
 pip install coverage
 coverage run script_test.py
 coverage report -m
 coverage xml -o coverage_report.xml
 coverage html -d coverage
-rmvirtualenv jenkins_test
--- run_pytest.bat
+++ run_pytest.bat
@@ -1,5 +1,2 @@
-call mkvirtualenv jenkins_test
 pip install pytest
 pytest --junit-xml=test_xml.xml
-call deactivate jenkins_test
-call rmvirtualenv jenkins_test
